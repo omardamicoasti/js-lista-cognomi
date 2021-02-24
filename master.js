c748fe696bb8b7737f06fcc3545dbb215f8a3cff
Omar D'Amico <omar.damico.asti@gmail.com>
2021-02-24 16:09:43 +0100

    Secondo push

--- master.js
+++ master.js
@@ -1,20 +1,21 @@
+// creazione array con i vari cognomi
 var listaCognomi = ["Bianchi", "Rossi", "Duzioni", "Balsano", "Verdi"];
-console.log(listaCognomi);
-
+// definizione variabile cognomeInput che mi serviva in seguito quando stampo
 var cognomeInput;
+// input
+listaCognomi.push(cognomeInput = prompt("Inserisci il tuo cognome con iniziale maiuscola"));
 
-listaCognomi.push(cognomeInput = prompt("Inserisci il tuo cognome"));
-console.log(cognomeInput);
-
+// lista da non ordinata ad ordinata
 var listaCognomiOrdinata;
 listaCognomiOrdinata = listaCognomi;
 listaCognomiOrdinata.sort();
 
+// stampa elenco nomi e posizione del cognome inserito in posizione umana e javascript
 for (var i = 0; i < listaCognomiOrdinata.length; i++) {
     var tmp = document.getElementById("listaCognomiOrdinata").innerHTML;
     document.getElementById("listaCognomiOrdinata").innerHTML = tmp + "<li>" + listaCognomiOrdinata[i] + "</li>";
     if (listaCognomiOrdinata[i]== cognomeInput ) {
-        document.getElementById("posizioneInput").innerHTML = "Il cognome " + cognomeInput + " è in posizione numero " + (i + 1);
+        document.getElementById("posizioneInput").innerHTML = "Il cognome " + cognomeInput + " è in posizione umana <b>" + (i + 1) + "</b> ed in posizione Javascript <b>" + i +"</b>";
     }
 }
 
